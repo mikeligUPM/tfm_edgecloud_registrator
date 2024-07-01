c67b4b29ba3c5fedb1fcda7ca1e0cc677e7c0e15
mikelig <mikelirazola@gmail.com>
2024-07-01 11:20:22 +0200

    Updated edge camera simulator for testing

--- src_edgeDevice/cameras_sim.py
+++ src_edgeDevice/cameras_sim.py
@@ -15,6 +15,10 @@ TOPIC = "cameraframes"
 
 SEND_FREQUENCY = 1  # Time in seconds between sending messages
 
+def save_json_to_local(payload, file_path):
+    with open(file_path, 'w') as json_file:
+        json.dump(payload, json_file, indent=4)
+
 def create_k_dict_by_camera(filepath) -> dict:
     k_dict = {}
     K = np.eye(3)
@@ -53,8 +57,8 @@ def build_publish_encoded_msg(client, frame_id, camera_name, encoded_color_image
     dt_now = datetime.now(tz=timezone.utc)
     send_ts = round(dt_now.timestamp() * 1000)
 
-    # encoded_color_image = "test"
-    # encoded_depth_image = "test"
+    # encoded_color_image = "test_color"
+    # encoded_depth_image = "test_depth"
     
     payload = {
         "frame_id": frame_id,
@@ -63,11 +67,14 @@ def build_publish_encoded_msg(client, frame_id, camera_name, encoded_color_image
         "enc_d": encoded_depth_image,
         "K": K,
         "send_ts": send_ts,
-        "target_model": "icp_p2l"  # either "icp_p2p", "icp_p2l" or "geotransformer"
+        "target_model": "icp_p2l"  # either "icp_p2p", "icp_p2l" or "geotrans"
     }
 
+    # save_json_to_local(payload, f"encoded_jsons\\{camera_name}_{frame_id}.json")
     client.publish(TOPIC, json.dumps(payload))
-    print(f"Sent message to IoT Hub: {payload}")
+    # print(f"Sent message to IoT Hub: {payload}")
+    print(f"Sent message to IoT Hub: {frame_id}")
+    time.sleep(0.5)
 
 
 # Function to process a frame
@@ -93,9 +100,11 @@ def main(client):
     base_directory = 'data\\png_data'
     k_dict = create_k_dict_by_camera("cam_params.json")
 
-    camera_dirs = [os.path.join(base_directory, d) for d in os.listdir(base_directory) if os.path.isdir(os.path.join(base_directory, d))]
     while True:
+        camera_dirs = [os.path.join(base_directory, d) for d in os.listdir(base_directory) if os.path.isdir(os.path.join(base_directory, d))]
+
         chosen_frame = random.choice(range(1, 219))  # Assuming frames are from 1 to 219
+        chosen_frame = 125
         print(f"Chosen frame: f{chosen_frame:04d}")
 
         threads = []
@@ -104,8 +113,10 @@ def main(client):
             thread = threading.Thread(target=process_frame, args=(client, k_dict, chosen_camera_dir, f"f{chosen_frame:04d}"))
             threads.append(thread)
             thread.start()
+            time.sleep(0.5)
 
-        time.sleep(5)  # Wait for 5 seconds before choosing another frame and closing threads
+        time.sleep(3)  # Wait for 5 seconds before choosing another frame and closing threads
+        x = input("Press enter to continue")
         # for thread in threads:
         #     thread.join()  # Wait for all threads to complete
 
