490b89ed541db4122023cda5b7c77f59407c0e5f
mikelig <mikelirazola@gmail.com>
2024-07-10 19:40:47 +0200

    Added environment vars to control test and prod. cleaned unusued code

--- src/helper_funs.py
+++ src/helper_funs.py
@@ -1,5 +1,6 @@
 import numpy as np
 from logger_config import logger
+import os
 
 def get_config(id_dataset):
     logger.debug(f"ID DATASET: {id_dataset}")
@@ -16,48 +17,12 @@ def get_config(id_dataset):
     return BACKBONE_INIT_VOXEL_SIZE, threshold
 
 
-def old_calculate_registration_metrics(pc_source, pc_ref, threshold, p2p_registration=False, final_result_print=False, i=1):
-    if final_result_print:
-        print(f"\n########## REGISTERED FINAL POINT CLOUD METRICS ############")
-    else:
-        print(f"\n############## ITERATION {i} METRICS ########################")
-    
-    # Calculate distances between corresponding points
-    distances = pc_source.compute_point_cloud_distance(pc_ref)
-
-    # Convert distances to a NumPy array for easy calculation
-    distances = np.asarray(distances)
-
-    # Calculate RMSE (Root Mean Square Error)
-    rmse = np.sqrt(np.mean(np.square(distances)))
-    print(f"### RMSE: {rmse}")
-
-    # Set a distance threshold to determine inliers
-    distance_threshold = threshold
-    print(f"### Distance Threshold: {distance_threshold}")
-
-    # Calculate Inlier Ratio
-    inliers = distances < distance_threshold
-    inlier_ratio = np.sum(inliers) / len(distances)
-    print(f"### Inlier Ratio: {inlier_ratio}")
-
-    # Calculate FMR (False Match Rate)
-    false_matches = distances >= distance_threshold
-    fmr = np.sum(false_matches) / len(distances)
-    print(f"### False Match Rate: {fmr}")
-
-    # Calculate Registration Recall
-    true_positives = np.sum(inliers)
-    total_correspondences = len(distances)  # Assuming the ground truth is that all points should be matched
-    registration_recall = true_positives / total_correspondences
-    print(f"### Registration Recall: {registration_recall}")
-
-    print("#################################################")
-
-
-
-
 def calculate_registration_metrics(pc_source, pc_ref, threshold, p2p_registration=False, final_result_print=False, i=1):
+    running_env = os.getenv('ENV', 'test')
+    if running_env != 'test':
+        logger.info(f"ENVIRONMENT is not test. Skipping metrics.")
+        return
+    
     """
     This function calculates the Root Mean Squared Error (RMSE), Inlier Ratio (IR), Recall Ratio (RR), 
     and False Match Ratio (FMR) between two point clouds represented by Open3d PointCloud objects.
--- src/logger_config.py
+++ src/logger_config.py
@@ -7,17 +7,19 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 # Create the full path for the log file
 log_file_path = os.path.join(script_dir, 'server.log')
 
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'DEBUG').upper()
 # Create a logger
 logger = logging.getLogger("server_logger")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(LOGLEVEL)
 
 # Create a console handler
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(LOGLEVEL)
 
 # Create a file handler
 fh = logging.FileHandler(log_file_path)
-fh.setLevel(logging.DEBUG)
+fh.setLevel(LOGLEVEL)
 
 # Create a formatter and set it for both handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
